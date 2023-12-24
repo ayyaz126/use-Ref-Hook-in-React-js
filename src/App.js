c98abd58226bb983fcf9eb51352e490def9348af
Ayyaz yousaf <yousafayyaz882@gmail.com>
2023-12-24 13:29:33 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useRef} from 'react';
 
 function App() {
+
+     let inputref = useRef(null)
+
+     function handleinput()
+     {
+      console.log("functin call")
+      inputref.current.value ="1000"
+      inputref.current.focus();
+      inputref.current.style.color="red"
+      inputref.current.style.display="none"
+     }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1> useRef in React js </h1>
+        <input type='text'  ref={inputref}/>
+        <button onClick={handleinput}  > Handle input </button>
     </div>
   );
 }
